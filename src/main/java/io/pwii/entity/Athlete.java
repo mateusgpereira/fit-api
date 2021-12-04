380798b406115d3ff11df74cae7e8af35ee71134
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2021-12-04 20:31:06 +0000

    creating instructor and athlete entities and migrations

---
+++ src/main/java/io/pwii/entity/Athlete.java
@@ -0,0 +1,22 @@
+package io.pwii.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Athlete extends User {
+  
+  @Column(length = 5)
+  private short height;
+
+  @Column(precision = 6, scale = 3)
+  private Double weight;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Instructor instructor;
+  
+}
---
+++ src/main/java/io/pwii/entity/User.java
@@ -0,0 +1,47 @@
+package io.pwii.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public class User extends PanacheEntityBase {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 50, nullable = false)
+  private String name;
+
+  @Column(length = 50, nullable = false, unique = true)
+  private String email;
+
+  @Column(length = 3)
+  private short age;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(length = 20, nullable = false)
+  private String phone;
+
+  @Column(length = 12, nullable = false, unique = true)
+  private String cpf;
+
+  @CreationTimestamp 
+  private LocalDate createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+  
+}
