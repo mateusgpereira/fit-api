be7f09725b9aaaabb25851e93597eef05784bf3e
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2022-04-23 21:01:05 +0100

    typo fixes setting up cors for dev add v1 to login endpoint

--- src/main/resources/keygen.sh
+++ src/main/resources/keygen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 openssl genrsa -out rsaPrivateKey.pem 2048
 
-openssl rsa -pubout -in rsaPrivateKey.pem -out publicKey.pem
+openssl rsa -pubout -in rsaPrivateKey.pem -out publickey.pem
 
-openssl pkcs8 -topk8 -nocrypt -inform pem -in rsaPrivateKey.pem -outform pem -out privateKey.pem
+openssl pkcs8 -topk8 -nocrypt -inform pem -in rsaPrivateKey.pem -outform pem -out privatekey.pem
 
 rm rsaPrivateKey.pem
